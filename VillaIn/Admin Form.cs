1efac5190887331b3d18c5f615f07e20c46e7ca8
mauraaninditakirana <kiranamaura351@gmail.com>
2025-04-23 11:56:39 +0700

    Menampilkan data ke dalam DataGridView saat form admin dimuat dan Menggunakan SqlDataAdapter dan DataTable untuk binding data.

--- VillaIn/Admin Form.cs
+++ VillaIn/Admin Form.cs
@@ -25,7 +25,12 @@ namespace VillaIn
             string connectionString = "Data Source=LAPTOP-08ADTL81\\MAURAANINDITAK;Initial Catalog=Villain
                  using (SqlConnection conn = new SqlConnection(connectionString))
             {
-
+                conn.Open();
+                string query = "SELECT * FROM Pengguna";
+                SqlDataAdapter da = new SqlDataAdapter(query, conn);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dataGridView1.DataSource = dt;
             }
         }
         private void label10_Click(object sender, EventArgs e)
